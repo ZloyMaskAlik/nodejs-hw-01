251a8e5a227c68d83392d33a608e1720d79c359c
Alex <usvplus@gmail.com>
2025-05-04 22:11:57 +0300

    nodes-hw-1-updated

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+  const contactList = await readContacts();
+  const contact = createFakeContact();
+  await writeContacts([...contactList, contact]);
+};
 
 addOneContact();
